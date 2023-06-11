2b11521d66151676f4245898fbaa83863eac84ae
Fabudi <evgeniydodg@gmail.com>
2023-06-11 20:19:11 +0300

    Initial commit

---
+++ app/src/main/java/inc/fabudi/design/MainActivity.kt
@@ -0,0 +1,11 @@
+package inc.fabudi.design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
